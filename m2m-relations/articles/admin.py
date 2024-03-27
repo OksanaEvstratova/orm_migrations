6722ad2ff641b761da2aa23e64d45ec50372434f
OksanaEvstratova <v.e.oksana@gmail.com>
2024-03-28 00:49:42 +1100

    m2m-relations added

--- m2m-relations/articles/admin.py
+++ m2m-relations/articles/admin.py
@@ -1,8 +1,31 @@
 from django.contrib import admin
+from django.forms import BaseInlineFormSet
+from .models import Article, Tag, Scope
+from django.core.exceptions import ValidationError
 
-from .models import Article
 
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    pass
+
+
+class RelationshipInlineFormset(BaseInlineFormSet):
+    def clean(self):
+        count = 0
+        for form in self.forms:
+            if 'is_main' in form.cleaned_data:
+                if form.cleaned_data['is_main']:
+                    count += 1
+            if count == 0:
+                raise ValidationError('Должен быть указан основной тэг')
+            else:
+                pass
+        return super().clean()
+
+class RelationshipInline(admin.TabularInline):
+    model = Scope
+    formset = RelationshipInlineFormset
 
 @admin.register(Article)
 class ArticleAdmin(admin.ModelAdmin):
-    pass
+    inlines = [RelationshipInline]
--- m2m-relations/articles/models.py
+++ m2m-relations/articles/models.py
@@ -1,5 +1,16 @@
 from django.db import models
 
+class Tag(models.Model):
+    name = models.CharField(max_length=50, verbose_name='Тэг')
+
+    class Meta:
+        verbose_name = 'Тэг'
+        verbose_name_plural = 'Тэги'
+        ordering = ['name']
+
+    def __str__(self):
+        return self.name
+
 
 class Article(models.Model):
 
@@ -7,10 +18,24 @@ class Article(models.Model):
     text = models.TextField(verbose_name='Текст')
     published_at = models.DateTimeField(verbose_name='Дата публикации')
     image = models.ImageField(null=True, blank=True, verbose_name='Изображение',)
+    scope = models.ManyToManyField(Tag, through='Scope')
 
     class Meta:
         verbose_name = 'Статья'
         verbose_name_plural = 'Статьи'
+        ordering = ['-published_at']
 
     def __str__(self):
         return self.title
+
+
+class Scope(models.Model):
+
+    article = models.ForeignKey(Article, on_delete=models.CASCADE, related_name='scopes')
+    tag = models.ForeignKey(Tag, on_delete=models.CASCADE)
+    is_main = models.BooleanField(default=False)
+
+    class Meta:
+        verbose_name = 'Тэг'
+        verbose_name_plural = 'Тэги'
+        ordering = ['is_main']
